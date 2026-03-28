ed67fb3922a3be7c0d3dbd9a58e078d6d376a746
Roy Dale Caliwag <157586512+roydale@users.noreply.github.com>
2026-03-28 19:39:46 +0800

    Updated scripts and created Lambda layer

--- back-end/post_meme.py
+++ back-end/post_meme.py
@@ -4,7 +4,7 @@ import time
 import base64, binascii
 import io
 from PIL import Image, UnidentifiedImageError
-### need to import the AWS SDK for Python here
+import boto3 # need to import the AWS SDK for Python here
 
 def lambda_handler(event, context):
     # event is a JSON string as described in https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-develop-integrations-lambda.html
@@ -20,10 +20,16 @@ def lambda_handler(event, context):
         header, data = meme_data["image"].split(";base64,")
         extension = header.split("image/")[-1]
     except ValueError:
-        pass ### respond with a status code 400 error and the string "badly-formed image data"
+        return {
+            "statusCode": 400,
+            "body": json.dumps("badly-formed image data")
+        } # respond with a status code 400 error and the string "badly-formed image data"
 
     if extension not in ("bmp", "gif", "jpeg", "png", "tiff"):
-        pass ### respond with a status code 400 error and the string "badly-formed image data"
+        return {
+            "statusCode": 400,
+            "body": json.dumps("badly-formed image data")
+        } # respond with a status code 400 error and the string "badly-formed image data"
 
     # use Pillow (https://pillow.readthedocs.io/en/stable/index.html)
     # to load the image (base 64 conversion code courtesy of
@@ -33,22 +39,25 @@ def lambda_handler(event, context):
     try:
         image = Image.open(io.BytesIO(base64.decodebytes(bytes(data, "utf-8"))))
     except (UnidentifiedImageError, binascii.Error):
-        pass ### respond with a status code 400 and the string "badly-formed image data"
+        return {
+            "statusCode": 400,
+            "body": json.dumps("badly-formed image data")
+        } # respond with a status code 400 and the string "badly-formed image data"
 
     # use a random UUID as the id for the meme (both the full image
     # and thumbnail)
     id = uuid.uuid4().hex
 
     # get the S3 bucket
-    s3 = ### get the service resource
-    bucket = ### get the sub-resource
+    s3 = boto3.resource("s3") # get the service resource
+    bucket = s3.Bucket("rc86-quantic-im-memes") # get the sub-resource
 
     # save it in an in-memory file-like object
     # if you're unfamiliar with the "with" statement, read this: https://www.geeksforgeeks.org/with-statement-in-python/
     with io.BytesIO() as in_mem_file:
         image.save(in_mem_file, format=image.format)
         in_mem_file.seek(0)
-        bucket. ### apply an action to upload the in-memory file to the /memes folder
+        bucket.upload_fileobj(in_mem_file, f"/memes/{id}") # apply an action to upload the in-memory file to the /memes folder
     
     # make a thumbnail and repeat the process above
     image.thumbnail((200, 200))
@@ -62,7 +71,7 @@ def lambda_handler(event, context):
     with io.BytesIO() as in_mem_file:
         image.save(in_mem_file, format="jpeg")
         in_mem_file.seek(0)
-        bucket. ### upload the in-memory file to the /thumbnails folder using the same id as the meme
+        bucket.upload_fileobj(in_mem_file, f"/thumbnails/{id}") # upload the in-memory file to the /thumbnails folder using the same id as the meme
 
     # write the entry to the database
     posted = int(time.time()) # current epoch time in seconds
@@ -74,9 +83,14 @@ def lambda_handler(event, context):
         "timeToDie": timeToDie
     }
     
-    dynamodb = ### get the service resource
-    table = ### get the sub-resource
-    table. ### apply the method to add an item to the table
+    dynamodb = boto3.resource("dynamodb") # get the service resource
+    table = dynamodb.Table("im-memes") # get the sub-resource
+    table.put_item(Item=db_entry)# apply the method to add an item to the table
 
-    ### respond with success code 200 and id of the meme
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            { "id": id }
+        )
+    } # respond with success code 200 and id of the meme
 
--- client/src/gateway.js
+++ client/src/gateway.js
@@ -1,2 +1,2 @@
-const gatewayUrl = undefined;
+const gatewayUrl = "https://lht8sue5o8.execute-api.us-east-1.amazonaws.com";
 export default gatewayUrl;
